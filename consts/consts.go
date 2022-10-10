730722e90fdd39fbd3a9d14cbf8012b4d314ce18
Tobeyw <tobey1024@126.com>
2022-10-10 16:04:06 +0800

    ipfs gateway

--- consts/consts.go
+++ consts/consts.go
@@ -3,5 +3,5 @@ package consts
 const (
 	Market_Main  = "0xd2e7cf18ee0d9b509fac02457f54b63e47b25e29"
 	Market_Test  = "0xc198d687cc67e244662c3b9c1325f095f8e663b1"
-	IPFS_GATEWAY = "ipfs.io"
+	IPFS_GATEWAY = "gateway.ipfs.io"
 )
